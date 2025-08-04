0e4d3ca47796ec44291e15c804a7afdf10a71331
Dicky Wahyu Ramadani <ramadani.wahyu@gmail.com>
2025-08-04 16:03:23 +0700

    perkara blm selesai

--- src/pages/Dashboard/Perkara/Perkara.jsx
+++ src/pages/Dashboard/Perkara/Perkara.jsx
@@ -1,16 +1,65 @@
-import React from 'react'
-import DashboardLayout from '../../../components/layouts/DashboardLayout'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import DashboardLayout from "../../../components/layouts/DashboardLayout";
+import { Link, useNavigate } from "react-router-dom";
+import axiosInstance from "../../../utils/axiosInstance";
+import { API_PATHS } from "../../../utils/apiPath";
+import { KLASIFIKASI_PERKARA } from "../../../utils/data";
+import { LuFilePlus } from "react-icons/lu";
 
 const Perkara = () => {
+  const [allPerkara, setAllPerkara] = useState([]);
+  const [tabs, setTabs] = useState([]);
+  const [filterKlasifikasi, setFilterKlasifikasi] = useState("All");
+
+  const navigate = useNavigate();
+
+  const getAllPerkara = async () => {
+    try {
+      const response = await axiosInstance.get(API_PATHS.PERKARA.ALL, {
+        params: {
+          klasifikasi: filterKlasifikasi === "All" ? "" : filterKlasifikasi,
+        },
+      });
+
+      setAllPerkara(
+        response.data?.perkara?.length > 0 ? response.data.perkara : []
+      );
+
+      const klasifikasiArray = KLASIFIKASI_PERKARA;
+      setTabs(klasifikasiArray);
+    } catch (error) {
+      console.error("Error memuat data Perkara", error);
+    }
+  };
+
+  const handleClick = (perkaraData) => {
+    navigate("dashboard/perkara/buat", {
+      state: { perkaraId: perkaraData._id },
+    });
+  };
+
+  useEffect(() => {
+    getAllPerkara(filterKlasifikasi);
+    return () => {};
+  }, [filterKlasifikasi]);
   return (
     <DashboardLayout activeMenu="Perkara">
-      <div className='card'>
-        <p className='flex p-5'>Perkara</p>
-        <Link className='p-5 btn-primary font-medium' to="buat">Tambah Perkara</Link>
+      <div className="my-5">
+        <div className="flex flex-col lg:flex-row lg:items-center justify-between">
+          <div className="flex items-center justify-between gap-3">
+            <h2 className="text-xl md:text-xl font-medium">Perkara</h2>
+            <button
+              className="lg:flex hidden create-btn"
+              onClick={() => navigate("buat")}
+            >
+              <LuFilePlus className="text-lg" /> Tambah Perkara
+            </button>
+          </div>
+        </div>
+        {/* <Link className='p-5 btn-primary font-medium' to="buat">Tambah Perkara</Link> */}
       </div>
     </DashboardLayout>
-  )
-}
+  );
+};
 
-export default Perkara
+export default Perkara;
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function NotFound() {
+  const navigate = useNavigate();
+
   return (
-    <div>NotFound</div>
-  )
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <div className="bg-white">
+          <div className="flex flex-col items-center">
+            <h1 className="font-bold text-3xl text-blue-600 lg:text-6xl">
+              404
+            </h1>
+
+            <h6 className="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
+              <span className="text-red-500">Oops!</span>
+            </h6>
+
+            <p className="mb-4 text-center text-gray-500 md:text-lg">
+              Halaman yang anda inginkan tidak ditemukan.
+            </p>
+            <div className="flex items-center justify-between">
+              <Link
+                to="/"
+                className="px-5 py-2 rounded-md text-blue-600 hover:text-blue-100 bg-blue-200 hover:bg-blue-600"
+              >
+                Dashboard
+              </Link>
+              <button
+                onClick={() => navigate(-1)}
+                className="ml-5 px-5 py-2 rounded-md text-green-600 hover:text-green-100 bg-green-200 hover:bg-green-600"
+              >
+                Kembali
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default NotFound
+export default NotFound;
