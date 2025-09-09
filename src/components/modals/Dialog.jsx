b84a2b66e074ef6d174c1aa25eb62fb23878eb82
Dicky Wahyu Ramadani <ramadani.wahyu@gmail.com>
2025-09-09 08:13:07 +0700

    import modal scrollable

--- src/components/modals/Dialog.jsx
+++ src/components/modals/Dialog.jsx
@@ -4,15 +4,17 @@ const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null; // Don't render if not open
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="bg-white p-6 rounded shadow-lg max-w-3xl w-full relative">
+    <div className="fixed inset-0 bg-gray-900/50 flex items-center justify-center z-50 overflow-auto overflow-x-hidden">
+      <div className="bg-white p-6 rounded shadow-lg overflow-scroll max-h-[90vh] w-full md:w-1/2 relative">
         <button
           onClick={onClose}
           className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 dark:hover:text-gray-300"
         >
           ✕
         </button>
-        {children} {/* This is where your modal content goes */}
+        <div className='overflow-y-auto pr-4 flex-grow mt-5'>
+          {children} {/* This is where your modal content goes */}
+        </div>
       </div>
     </div>
   );
--- src/pages/Dashboard/Perkara/Perkara.jsx
+++ src/pages/Dashboard/Perkara/Perkara.jsx
@@ -1,18 +1,35 @@
 import React, { useEffect, useState } from "react";
-import DashboardLayout from "../../../components/layouts/DashboardLayout";
+import * as XLSX from 'xlsx';
 import { Link, useNavigate } from "react-router-dom";
+
+import DashboardLayout from "../../../components/layouts/DashboardLayout";
 import axiosInstance from "../../../utils/axiosInstance";
 import { API_PATHS } from "../../../utils/apiPath";
 import { KLASIFIKASI_PERKARA } from "../../../utils/data";
-import { LuFilePlus } from "react-icons/lu";
+import { LuFilePlus, LuFileSpreadsheet } from "react-icons/lu";
 import PerkaraCard from "../../../components/cards/PerkaraCard";
 import PerkaraTabs from "../../../components/tabs/PerkaraTabs";
+import Modal from "../../../components/modals/Dialog";
 
 const Perkara = () => {
   const [allPerkara, setAllPerkara] = useState([]);
   const [tabs, setTabs] = useState([]);
   const [filterKlasifikasi, setFilterKlasifikasi] = useState("Perdata Gugatan");
   const [loading, setLoading] = useState(false)
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [limit, setLimit] = useState(10)
+
+  const [importData, setImportData] = useState([])
+
+  const [isOpenImportFromExcell, setIsOpenImportFromExcell] = useState(false);
+  const closeImportFromExcell = () => {
+    setImportData([])
+    setIsOpenImportFromExcell(false)
+  }
+  const openImportFromExcell = () => {
+    setIsOpenImportFromExcell(true);
+  }
 
   const navigate = useNavigate();
 
@@ -23,6 +40,8 @@ const Perkara = () => {
       const response = await axiosInstance.get(API_PATHS.PERKARA.ALL, {
         params: {
           klasifikasi: filterKlasifikasi === "Perdata Gugatan" ? "Perdata Gugatan" : filterKlasifikasi,
+          page: currentPage,
+          limit: limit
         },
       });
       setAllPerkara(response.data?.perkara?.length > 0 ? response.data?.perkara : []);
@@ -30,10 +49,28 @@ const Perkara = () => {
       const klasifikasiArray = KLASIFIKASI_PERKARA;
       setTabs(klasifikasiArray);
     } catch (error) {
+      console.log(error)
       console.error("Error memuat data Perkara", error);
       setLoading(false);
     } finally {
       setLoading(false);
+    }
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const bufferArray = e.target.result;
+        const workbook = XLSX.read(bufferArray, { type: 'buffer' });
+        const sheetName = workbook.SheetNames[0]; // Get the first sheet
+        const worksheet = workbook.Sheets[sheetName];
+        const data = XLSX.utils.sheet_to_json(worksheet);
+        console.log(data)
+        setImportData(data);
+      };
+      reader.readAsArrayBuffer(file);
     }
   };
 
@@ -64,7 +101,13 @@ const Perkara = () => {
             setActiveTab={setFilterKlasifikasi}
           />
         </div>
-        <div className="flex item-center justify-end">
+        <div className="flex item-center justify-end gap-4">
+          <button
+            className="lg:flex hidden create-btn"
+            onClick={() => openImportFromExcell()}
+          >
+            <LuFileSpreadsheet className="text-lg" /> Import Data
+          </button>
           <button
             className="lg:flex hidden create-btn"
             onClick={() => navigate("buat")}
@@ -83,6 +126,46 @@ const Perkara = () => {
           />
         ))}
       </div>
+
+      <Modal isOpen={isOpenImportFromExcell} onClose={closeImportFromExcell}>
+        <div className="flex items-center justify-between">
+          <h2 className="text-xl font-medium mb-4">Impor Data</h2>
+          <p className="text-xs font-light">Mengimpor data perkara dari file .xlxs atau .xls</p>
+        </div>
+        <div className="mt-4">
+          <div className="mt-1">
+            <input type="file" accept=".xlsx, .xls" onChange={handleFileChange} />
+          </div>
+          <div className="mt-1">
+            {importData.length > 0 && (
+              <table className="">
+                <thead className="border-b-2">
+                  <tr>
+                    <th>No</th>
+                    <th>Tanggal Pendaftaran</th>
+                    <th>Klasifikasi</th>
+                    <th>Jenis Perkara</th>
+                    <th>Nomor Perkara</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {importData.map((row, index) => (
+                    <tr key={index} className="border-b-2">
+                      <td>{row.no}</td>
+                      <td>{row.tglDaftar}</td>
+                      <td>{row.klasifikasi}</td>
+                      <td>{row.jenis}</td>
+                      <td>{row.nomor}/{row.kodePerkara}/{row.tahun}/{row.kodeSatker}</td>
+                      <td></td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
+          </div>
+        </div>
+      </Modal>
 
     </DashboardLayout>
   );
