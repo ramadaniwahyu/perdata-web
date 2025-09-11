40fafb785079475c7f34471316d20cc3fe790eea
Dicky Wahyu Ramadani <ramadani.wahyu@gmail.com>
2025-09-12 05:36:31 +0700

    api path using ip public

--- src/utils/apiPath.js
+++ src/utils/apiPath.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://192.168.3.222:8010/api";
+export const BASE_URL = "http://27.124.88.70:8010/api";
 // export const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const API_PATHS ={
