4cc177b5a0802919c7c5bdb2c7156d3ac34cccd8
Dicky Wahyu Ramadani <ramadani.wahyu@gmail.com>
2025-09-12 07:22:07 +0700

    usi

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { BASE_URL } from "./apiPath";
+// import { BASE_URL } from "./apiPath";
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.BASE_URL,
