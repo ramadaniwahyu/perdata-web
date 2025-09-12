33043208c6d5d8fc5079cc8f4befeb03eae81362
Dicky Wahyu Ramadani <ramadani.wahyu@gmail.com>
2025-09-12 07:05:14 +0700

    using base url access

--- src/utils/apiPath.js
+++ src/utils/apiPath.js
@@ -5,48 +5,48 @@
 
 export const API_PATHS ={
     AUTH: {
-        REGISTER: "/api/register",
-        LOGIN: "/api/login",
-        LOGOUT: "/api/logout",
-        PROFILE: "/api/profile",
-        REFRESH_TOKEN: "/api/refresh_token"
+        REGISTER: "/register",
+        LOGIN: "/login",
+        LOGOUT: "/logout",
+        PROFILE: "/profile",
+        REFRESH_TOKEN: "/refresh_token"
     },
     USERS: {
-        ALL: (search) => `/api/users${search}`,
-        ONE: (userId) => `/api/users/${userId}`,
-        ACTIVATE: (userId) => `/api/users/${userId}/activate`,
-        DELETE: (userId) => `/api/users/${userId}/delete`,
+        ALL: (search) => `/users${search}`,
+        ONE: (userId) => `/users/${userId}`,
+        ACTIVATE: (userId) => `/users/${userId}/activate`,
+        DELETE: (userId) => `/users/${userId}/delete`,
     },
     PERKARA: {
-        ALL: "/api/perkara",
-        NOMOR: "/api/perkara/nomor",
-        ONE: (perkaraId) => `/api/perkara/${perkaraId}`,
-        DELETE: (perkaraId) => `/api/perkara/${perkaraId}/delete`,
-        RIWAYAT: (perkaraId) => `/api/perkara/${perkaraId}/riwayat`,
+        ALL: "/perkara",
+        NOMOR: "/perkara/nomor",
+        ONE: (perkaraId) => `/perkara/${perkaraId}`,
+        DELETE: (perkaraId) => `/perkara/${perkaraId}/delete`,
+        RIWAYAT: (perkaraId) => `/perkara/${perkaraId}/riwayat`,
     },
     JURUSITA: {
-        GET: "/api/jurusita-all",
-        ALL: "/api/jurusita",
-        ONE: (jurusitaId) => `/api/jurusita/${jurusitaId}`,
-        DELETE: (jurusitaId) => `/api/jurusita/${jurusitaId}/delete`,
+        GET: "/jurusita-all",
+        ALL: "/jurusita",
+        ONE: (jurusitaId) => `/jurusita/${jurusitaId}`,
+        DELETE: (jurusitaId) => `/jurusita/${jurusitaId}/delete`,
     },
     RELAAS: {
         ALL: `/panggilan`,
-        ALL_SEARCH: (search) => `/api/panggilan${search}`,
-        ONE: (panggilanId) => `/api/panggilan/${panggilanId}`,
-        DELETE: (panggilanId) => `/api/panggilan/${panggilanId}/delete`,
-        CANCEL_KIRIM: (panggilanId) => `/api/panggilan/${panggilanId}/pengiriman`,
-        CANCEL_PELAKSANAAN: (panggilanId) => `/api/panggilan/${panggilanId}/pelaksanaan`,
+        ALL_SEARCH: (search) => `/panggilan${search}`,
+        ONE: (panggilanId) => `/panggilan/${panggilanId}`,
+        DELETE: (panggilanId) => `/panggilan/${panggilanId}/delete`,
+        CANCEL_KIRIM: (panggilanId) => `/panggilan/${panggilanId}/pengiriman`,
+        CANCEL_PELAKSANAAN: (panggilanId) => `/panggilan/${panggilanId}/pelaksanaan`,
     },
     JENIS_PANGGILAN: {
-        ALL: "/api/jenis-panggilan",
-        ONE: (panggilanId) => `/api/jenis-panggilan/${panggilanId}`,
-        DELETE: (panggilanId) => `/api/jenis-panggilan/${panggilanId}/delete`,
+        ALL: "/jenis-panggilan",
+        ONE: (panggilanId) => `/jenis-panggilan/${panggilanId}`,
+        DELETE: (panggilanId) => `/jenis-panggilan/${panggilanId}/delete`,
     },
     IMAGE: {
-        UPLOAD: "/api/upload-image",
+        UPLOAD: "/upload-image",
     },
     UPLOAD: {
-        FILE: "/api/upload-file"
+        FILE: "/upload-file"
     }
 }
