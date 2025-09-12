087598fd8779d18f3af4d3ad22902d597ece3ad5
Dicky Wahyu Ramadani <ramadani.wahyu@gmail.com>
2025-09-12 07:14:08 +0700

    Revert "using base url access"
    This reverts commit d47c0e06757eee8cc042ab38bdb623e35ad010eb.

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-// import { BASE_URL } from "./apiPath";
+import { BASE_URL } from "./apiPath";
 
 const axiosInstance = axios.create({
-    // baseURL: BASE_URL,
-    baseURL: import.meta.env.BASE_URL,
+    baseURL: BASE_URL,
+    // baseURL: import.meta.env.API_URL,
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
